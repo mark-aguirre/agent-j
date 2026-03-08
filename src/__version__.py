cfe0a48a21ab24c1e39e9ea297cd94683ba8e3f8
mark-aguirre <markaguirre26@gmail.com>
2026-03-08 15:41:01 +0800

    Release v2.0.9

--- src/__version__.py
+++ src/__version__.py
@@ -2,7 +2,7 @@
 AgentJ Trading Bot Version Information
 """
 
-__version__ = "2.0.8"
+__version__ = "2.0.9"
 __app_name__ = "AgentJ Trading Bot"
 __author__ = "AgentJ Team"
 __description__ = "Discord Trading Bot with MT5 Integration"
